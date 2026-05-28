ad4ecfa4b2ccf84f3e38ced33e94b933fc9181cf
dmooshm <david_moos@horacemann.org>
2026-05-28 00:16:45 -0400

    protein powder gifts

--- Stardew Santa/Class1.cs
+++ Stardew Santa/Class1.cs
@@ -1,5 +1,29 @@
-﻿namespace Stardew_Santa;
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
 
-public class Class1
+namespace StardewSanta
 {
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+        }
+
+        private void OnDayStarted(object? sender, DayStartedEventArgs e)
+        {
+            // Winter 25 = Christmas!
+            if (Game1.currentSeason == "winter" && Game1.dayOfMonth == 25)
+            {
+                var item = ItemRegistry.Create("(O)david.proteinPowder_ProteinPowder", 1);
+
+                if (item != null)
+                {
+                    Game1.player.addItemByMenuIfNecessary(item);
+                    Game1.addHUDMessage(new HUDMessage("Merry Christmas! Santa gifted you some protein powder!", HUDMessage.newQuest_type));
+                }
+            }
+        }
+    }
 }
